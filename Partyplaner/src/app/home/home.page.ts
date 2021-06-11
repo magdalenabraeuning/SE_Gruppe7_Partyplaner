d829aa83bb7f4acfe0a754abe356a7a2d0a6dd72
annimareen <annika.mareen@googlemail.com>
2021-06-11 12:41:21 +0200

    speichern von Datensätzen

--- Partyplaner/src/app/home/home.page.ts
+++ Partyplaner/src/app/home/home.page.ts
@@ -26,7 +26,10 @@ export class HomePage {
   }
 
 
-  addParty(){
+  async addParty(){
+    let userID = (await this.afAuth.currentUser.then((user)=>{return user.uid;}));
+    console.log(userID);
+    
     this.alertCtrl.create({
       message: "Party erstellen",
       inputs:[
@@ -36,7 +39,16 @@ export class HomePage {
       buttons:[
         {
           text: 'Add', 
-          handler:() => {
+          handler:(res) => {
+            console.log(res);
+            this.afFirestore.collection(userID).add({
+              //statt userID oben "Partys" -> alle Partys auf der Ebene darunter
+              
+              title: res.title,
+              desc: res.desc,
+              createdAt: Date.now(),
+              isDone: false
+            });
 
           }
         }, {
