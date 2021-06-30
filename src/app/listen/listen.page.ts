db9849cdbd6e5d172f990dfc8e9153580bf4f3cc
JaninaCol <55322002+JaninaCol@users.noreply.github.com>
2021-06-30 17:22:19 +0200

    ordner neu vom main reinkopiert

--- src/app/listen/listen.page.ts
+++ src/app/listen/listen.page.ts
@@ -1,4 +1,7 @@
+import { isNgTemplate } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import {​​​ AlertController }​​​ from'@ionic/angular';
+
 
 @Component({
   selector: 'app-listen',
@@ -7,7 +10,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListenPage implements OnInit {
 
-  constructor() { }
+  toDoList = [{
+    itemName:"Chips",
+    itemMenge: "1Kg",
+    itemCategory:"Essen",
+    itemUser:"Lena"
+
+  },
+  {
+    itemName:"Nüsse",
+    itemMenge: "1Kg",
+    itemCategory:"Essen",
+    itemUser:"Annika"
+  }
+]
+
+  constructor(private alertCtrl: AlertController) {}
+
+  addItem(){
+    this.alertCtrl.create({
+      message: "Item hinzufügen",
+      inputs: [
+        { type: 'text', name: 'itemName', placeholder: "Name" },
+        { type: 'textarea', name: 'itemMenge', placeholder: "Menge" },
+        { type: 'textarea', name: 'itemCategory', placeholder: "Kategorie" },
+        { type: 'textarea', name: 'itemUser', placeholder: "User" }
+      ],
+      buttons: [
+        {
+          text: 'Add',
+          handler: (res) => {
+            console.log(res);
+            this.toDoList.push(res);
+          }
+        }, {
+          text: 'Cancel'
+        }
+      ]
+    }).then(a => a.present());
+  }
 
   ngOnInit() {
   }
