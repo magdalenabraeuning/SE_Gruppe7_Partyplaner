6de28430dc01089ff0d491ce2210824c3af6db44
JaninaCol <55322002+JaninaCol@users.noreply.github.com>
2021-07-01 22:00:23 +0200

    Cypress - erster Test der auf unsere App zugreift

--- cypress/integration/1-getting-started/todo.spec.js
+++ cypress/integration/1-getting-started/todo.spec.js
@@ -4,7 +4,7 @@
 //
 // This spec file contains a variety of sample tests
 // for a todo list app that are designed to demonstrate
-// the power of writing tests in Cypress.
+// the power of writing tests in Cypress. 
 //
 // To learn more about how Cypress works and
 // what makes it such an awesome testing tool,
