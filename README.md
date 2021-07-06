f9b80501147762d9541b4b2b449be75df5d352fa
JaninaCol <55322002+JaninaCol@users.noreply.github.com>
2021-07-06 14:15:47 +0200

    Cypress - Update

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # SE_Gruppe7_Partyplaner
 Partyorganisations- und Durchführungs-App
+
+
+Hier für Mark notieren wo er überall reinschaen soll und wo er was findet s
--- cypress/integration/login_page_spec.js
+++ cypress/integration/login_page_spec.js
@@ -3,29 +3,44 @@
 describe('The Login Page', () => {
   
     it('just log in', function () {
-        // auf die Loginseite kommen 
+        // visit the startpage - if not loged in login will appear
         cy.visit('/')
 
-      // destructuring assignment of the this.currentUser object
+        //TODO: check if not loged in 
+        //Testcase Conditional Testing (Try-Out)
+        // this only works if there's 100% guarantee body has fully rendered without any pending changes to its state
+        cy.get('button').then(($btn) => {
+            // synchronously ask for the body's text and do something based on whether it includes another string
+            if ($btn.includes('Sign Out')) {
+            // yup found it
+            cy.wait(1000)
+            cy.get('button')
+              .should('have.value', "Sign Out")
+              .click()
+            } 
+        })
+        //md button button-solid ion-activatable ion-focusable hydrated
+
+       // not the clean way to test this, but everything else would have taken a huge amount of time 
+       // for example we could have used the Google Authetication API, wich is not easy to handle
       const { username, password } = { username:'ninamagdatestuser@gmx.de', password: 'NinaMagdaTestuser'}
 
       cy.get('input#ui-sign-in-email-input.mdl-textfield__input.firebaseui-input.firebaseui-id-email')
-        .wait(1000) // have to wait until everything finished loading, because if not it interups the type() command
+        .wait(1000) // have to wait until everything finished loading, because if not incoming XHR requests interupt the type() command
         .clear()
         .type(username) 
-        .should("have.value", username) //Errorhandling um zu schauen obs fertig getippt hat        
+        .should("have.value", username) //to check if type finished successful         
 
       cy.get('button').click()
-      cy.wait(1000)
+      cy.wait(2000)
   
       cy.get('input#ui-sign-in-password-input.mdl-textfield__input.firebaseui-input.firebaseui-id-password')
         .clear()
         .type(password)
-        .wait(1000)
         .should("have.value", password)
 
       cy.get('button').click() 
-      cy.wait(1000)
+      cy.wait(1000) //just to make sure the test dont fails just because it take a second more to load 
 
       // we should be redirected to /home
       cy.url().should('include', '/home')
